f1dde5dd5dca7eb774d28ffc2a8ac1a9a153af2c
IzuOvando <voja210394@gmail.com>
2024-07-09 22:28:57 -0600

    solve_delete_many

--- src/actions/convert.ts
+++ src/actions/convert.ts
@@ -255,7 +255,7 @@ class FileProcessor {
                     data: {
                         empresa: record.empresa,
                         material: record.material,
-                        cubicacion: `${record.cubicacion} m3`,
+                        cubicacion: `${record.cubicacion} m³`,
                         fecha: record.fecha,
                         placas: record.placas,
                         idCamion: record.idCamion,
