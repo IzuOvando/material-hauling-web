f0bdf47f6a3c9d5790b18ddf727638bd42fb2dac
Ruben35 <ruben111416@gmail.com>
2024-07-26 10:15:19 -0600

    ✏️ Rewrite on lowercase()

--- src/app/api/files/downloadbbd/route.ts
+++ src/app/api/files/downloadbbd/route.ts
@@ -6,39 +6,46 @@ import { promisify } from "util";
 const readFile = promisify(fs.readFile);
 
 export async function POST(req: NextRequest) {
-    if (req.method !== "POST") {
-        return new Response(JSON.stringify({ error: "Method Not Allowed" }), {
-            status: 405,
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-    }
+  if (req.method !== "POST") {
+    return new Response(JSON.stringify({ error: "Method Not Allowed" }), {
+      status: 405,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 
-    const data = await req.json();
-    const { nombre, area } = data;
+  const data = await req.json();
+  const { nombre, area }: { nombre: string; area: string } = data;
 
-    const rootPath = path.resolve(process.cwd());
-    const specificFilePath = path.join(rootPath, "db_output", "excel", nombre, `${area}.xlsx`);
+  const rootPath = path.resolve(process.cwd());
+  const specificFilePath = path.join(
+    rootPath,
+    "db_output",
+    "excel",
+    nombre,
+    `${area.toLowerCase()}.xlsx`
+  );
 
-    try {
-        const fileBuffer = await readFile(specificFilePath);
-        const headers = new Headers({
-            "Content-Type": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-            "Content-Disposition": `attachment; filename="${area}.xlsx"`
-        });
+  try {
+    const fileBuffer = await readFile(specificFilePath);
+    const headers = new Headers({
+      "Content-Type":
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      "Content-Disposition": `attachment; filename="${area}.xlsx"`,
+    });
 
-        return new Response(fileBuffer, {
-            status: 200,
-            headers: headers
-        });
-    } catch (error) {
-        console.error("Error reading file:", error);
-        return new Response(JSON.stringify({ error: "Failed to read file" }), {
-            status: 500,
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-    }
+    return new Response(fileBuffer, {
+      status: 200,
+      headers: headers,
+    });
+  } catch (error) {
+    console.error("Error reading file:", error);
+    return new Response(JSON.stringify({ error: "Failed to read file" }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 }
