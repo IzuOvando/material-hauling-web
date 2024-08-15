29b61c1c8379c399f7990429806cf100253a6e9f
IzuOvando <voja210394@gmail.com>
2024-08-14 21:24:17 -0600

    sapace

--- src/actions/convert.ts
+++ src/actions/convert.ts
@@ -10,6 +10,8 @@ import { CreateTicketDto, filteredDataConfig, isCreateAcarreosDto, isCreateGasol
 import CONFIG from "@/config";
 
 
+
+
 class FileProcessor {
 
     private toCamelCase(str: string): string {
