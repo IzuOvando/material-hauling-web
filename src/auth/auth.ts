b9df0b79e4295c4306f83a9be9a7e5b610e47b94
IzuOvando <70975398+IzuOvando@users.noreply.github.com>
2024-09-09 14:14:09 -0600

    encrypt (#55)
    * encrypt
    
    * delete validation 6

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 import Credentials from "next-auth/providers/credentials";
-import { SHA256 as sha256 } from "crypto-js";
 import prisma from "@/lib/db";
 
 export const { auth, signIn, signOut } = NextAuth({
@@ -11,17 +10,14 @@ export const { auth, signIn, signOut } = NextAuth({
       async authorize(credentials, request) {
         const userCredentials = {
           username: credentials?.username as string,
-          password: credentials?.password as string,
+          password: credentials?.password as string, // Ya está hasheada desde el cliente
         };
 
         const user = await prisma.user.findUnique({
           where: { username: userCredentials.username },
         });
 
-        if (
-          user &&
-          user.password === sha256(userCredentials.password).toString()
-        )
+        if (user && user.password === userCredentials.password)
           return { name: user.username };
         else return null;
       },
