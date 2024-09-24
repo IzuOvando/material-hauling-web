d1544b60eea68d85cda4cdc7f9d796b7081704ab
IzuOvando <voja210394@gmail.com>
2024-09-24 15:31:41 -0600

    date

--- src/lib/schemas/csv_schemas.tsx
+++ src/lib/schemas/csv_schemas.tsx
@@ -86,8 +86,8 @@ export const filteredDataConfig: Record<
     const hora = parseHoraTime(cleanQuotes(data.hora))
 
     const fechaStr = cleanQuotes(data.fecha);
-    
-    const [day, month, year] = fechaStr.split('-').map(part => parseInt(part, 10));
+
+    const [month, day, year] = fechaStr.split('-').map(part => parseInt(part, 10));
 
     const fecha = new Date(year, month - 1, day);
 
