8183fe8d0ef56dd50ff905b377ea88f3f1badf81
IzuOvando <70975398+IzuOvando@users.noreply.github.com>
2025-02-24 09:58:33 -0600

    Asfalto printer (#92)
    * add_asfalto_section
    
    * add_asfalto_section
    
    * tickets asfalto
    
    * upload nrv img and \n

--- src/lib/printers/schemas/index.ts
+++ src/lib/printers/schemas/index.ts
@@ -2,3 +2,4 @@ export type { default as TicketSchema } from "./TicketSchema";
 export { default as TicketAcarreoSchema } from "./TicketAcarreoSchema";
 export { default as TicketGasolinaSchema } from "./TicketGasolinaSchema";
 export { default as TicketConcretoSchema } from "./TicketConcretoSchema";
+export { default as TicketAsfaltoSchema } from "./TicketAsfaltoSchema";
