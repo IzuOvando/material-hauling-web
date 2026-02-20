d5051477b79ea01a45ce9c5c8f73b58f85aa1929
IzuOvando <voja210394@gmail.com>
2026-02-20 12:55:20 -0600

    Fix formatting of faceted filter

--- src/auth/auth.config.ts
+++ src/auth/auth.config.ts
@@ -1,6 +1,8 @@
 // TODO (SEC-015): Migrate to next-auth v5 stable when released. Currently on beta.30.
 import type { NextAuthConfig } from "next-auth";
 
+
+
 export const authConfig = {
   providers: [],
   session: { strategy: "jwt", maxAge: 24 * 60 * 60 },
