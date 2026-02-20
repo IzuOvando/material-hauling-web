d6b7f7099664d46b679470a67303c721a822fcaf
IzuOvando <voja210394@gmail.com>
2026-02-20 12:59:17 -0600

    SDN-72/fix/repeated-facets

--- src/auth/auth.config.ts
+++ src/auth/auth.config.ts
@@ -1,8 +1,6 @@
 // TODO (SEC-015): Migrate to next-auth v5 stable when released. Currently on beta.30.
 import type { NextAuthConfig } from "next-auth";
 
-
-
 export const authConfig = {
   providers: [],
   session: { strategy: "jwt", maxAge: 24 * 60 * 60 },
